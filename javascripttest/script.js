49e1b8ed57f5667d3cb58c1e590b9d8b5ce14d8b
tan <materrazas076@gmail.com>
2023-09-25 18:37:47 -0700

    Intro to JavaScript

---
+++ javascripttest/script.js
@@ -0,0 +1,33 @@
+console.log("Happy is there!")
+
+// guh
+/* 
+guh
+guh
+guh 
+*/
+const PI = 3.14;
+let name = "Ethan"
+name = "Happy"
+console.log(name)
+
+const age = 5 + 4
+console.log(age, name)
+
+function add(a,b) {
+    return a + b;
+}
+
+const add2 = (a,b) => a+b;
+
+console.log(add(1, 4))
+
+const car ={
+    model:"Camry",
+    color:"White",
+    mileage: 1000
+}
+
+car.model = "Mustang"
+
+console.log(car.model)
