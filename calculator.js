353c333abade4ed9d4aff8198818315f7aa25d71
tan <materrazas076@gmail.com>
2023-10-03 10:50:40 -0700

    Arithmetic Work (incomplete)

--- calculator.js
+++ calculator.js
@@ -1,17 +1,44 @@
-function getOperation() {
-    const number = event.currentTarget.innerText;
-    
+function displayOperation() {
+    let answer = "";
+    const n = ("Choose a Number");
+    let operation = event.currentTarget.innerText;                    // get text from buttons
+    if (document.getElementById("calcDisplay").value == "0") {          // if 0 is still displayed
+        document.getElementById("calcDisplay").value = operation;           // replace it with input
+    } 
+    else {
+        document.getElementById("calcDisplay").value += operation;
+    }
+    if (document.getElementById("calcDisplay").value.includes("=")) {
+        doOperation(operation);
+    }
 }
 
-function setOperation() {
+function doOperation(o) {
+    let func = o;
+    let a = "";
+    let b = "";
+    let add = o.search("+");
+    let sub = o.search("-");
+    let mul = o.search("*");
+    let div = o.search("/");
+    if (Boolean(add)) {
+        const funcOperation = text.split("+");
+        a = funcOperation[0];
+        b = funcOperation[1];
+        
+    } else if (Boolean(sub)) {
 
+    } else if (Boolean(mul)) {
+
+    } else if (Boolean(div)) {
+
+    } else {
+        return "error";
+    }
+    document.getElementById("demo").innerHTML = myArray[1]; 
 }
 
-function doOperation(a, o, b) {
-
-}
-
-function displayOperation (an) {
+function displayAnswer (an) {
     
 }
 
