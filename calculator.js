c28353b1292cd79718335a5eca0eef0930f24ca2
tan <materrazas076@gmail.com>
2023-10-02 18:01:43 -0700

    Start of buttons

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function getOperation() {
+    const number = event.currentTarget.innerText;
+    
+}
+
+function setOperation() {
+
+}
+
+function doOperation(a, o, b) {
+
+}
+
+function displayOperation (an) {
+    
+}
+
+function clear() {
+    
+}
