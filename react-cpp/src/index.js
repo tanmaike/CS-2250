965e388d10503831ed0046c7deaa2dafb022cd40
tan <materrazas076@gmail.com>
2023-11-21 20:24:41 -0800

    React HW6 Complete

--- react-cpp/src/index.js
+++ react-cpp/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import "./styles.css";
 import App from "./App";
+import API from "./API";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <API />
   </React.StrictMode>
 );
 
