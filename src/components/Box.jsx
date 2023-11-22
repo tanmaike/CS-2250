d7b14485fd6a0a402277f6439f86845ddc4d05ee
tan <materrazas076@gmail.com>
2023-11-21 20:35:32 -0800

    New attempt

--- src/components/Box.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./Box.css"; 
-
-export const Box = (props) => {
-  return (
-    <div className="Box" style={{backgroundColor: props.background, border: `2px solid ${props.borderColor}` }}><h1>{props.text}</h1></div>
-  )
-}
